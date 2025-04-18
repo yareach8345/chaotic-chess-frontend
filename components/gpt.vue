a30da9505b138855e584fc76905c780b90ab2c54
yareach8345 <mine8345@naver.com>
2025-04-18 10:49:22 +0900

    add gpt icon
    화면 상단에 보여줄 상대방(GPT)의 아이콘을 추가
    Message를 보여줄 수도 있게 만들었다.

---
+++ components/gpt.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface Props {
+  message?: string
+  showMessage?: boolean
+}
+
+const props = withDefaults(
+    defineProps<Props>(),
+    {
+      message: "",
+      showMessage: true,
+    }
+)
+
+const showMessage = computed(() => {
+  const isMessageNotEmpty = props.message.length > 0
+  return isMessageNotEmpty && props.showMessage
+})
+</script>
+
+<template>
+  <div class="d-flex flex-column align-center position-relative">
+    <v-img src="/ChatGPT_logo.svg" width="10%" alt="gpt"/>
+    <v-expand-transition>
+      <v-card elevation="0" class="position-absolute bottom-0 pa-sm-1" border="thin" v-if="showMessage" color="info">
+        {{props.message}}
+      </v-card>
+    </v-expand-transition>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
