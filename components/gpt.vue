40a2d95dd95ac671826be2b68ce364509152b15a
yareach8345 <mine8345@naver.com>
2025-04-24 00:55:36 +0900

    feat: gpt 레이아웃 수정
    GPT의 메시지가 아이콘을 가리지 않도록 수정함

--- components/gpt.vue
+++ components/gpt.vue
@@ -19,7 +19,7 @@ const showMessage = computed(() => {
 </script>
 
 <template>
-  <div class="d-flex flex-column align-center position-relative">
+  <div class="d-flex flex-column align-center position-relative pb-10">
     <v-img src="/ChatGPT_logo.svg" width="10%" alt="gpt"/>
     <v-expand-transition>
       <v-card elevation="0" class="position-absolute bottom-0 pa-sm-1" border="thin" v-if="showMessage" color="info">
