f213c2798f9951cf51cbe557e52c75df318224fc
yareach8345 <mine8345@naver.com>
2025-04-30 23:58:05 +0900

    add 스네이크 케이스 to 카멜 케이스 유틸 추가
    - fastapi의 스네이크 케이스의 응답을 카멜케이스로 바꾸기 위한 함수 추가

---
+++ utils/caseTransformUtils.ts
@@ -0,0 +1,31 @@
+const convertSnakeCaseToCamelCase = (str: string): string => {
+    const words = str.split('_');
+    return words
+        .map((word, index) => {
+            if (index === 0) {
+                return word;
+            }
+            const firstLetterCap = word.charAt(0).toUpperCase();
+            const remainingLetters = word.slice(1);
+            return firstLetterCap + remainingLetters;
+        })
+        .join('');
+};
+
+const convertObjectPropertiesSnakeCaseToCamelCase = (
+    obj: Record<string, any>,
+): Record<string, any> => {
+    const convertedObject: Record<string, any> = {};
+    for (let [key, value] of Object.entries(obj)) {
+        const camelCaseKey = convertSnakeCaseToCamelCase(key);
+        if (!Array.isArray(value) && typeof value === 'object' && value !== null && value !== undefined) {
+            value = convertObjectPropertiesSnakeCaseToCamelCase(value);
+        }
+        convertedObject[camelCaseKey] = value;
+    }
+    return convertedObject;
+};
+
+export const toCamelCase = <T>(obj: any) => {
+    return convertObjectPropertiesSnakeCaseToCamelCase(obj) as T;
+}
